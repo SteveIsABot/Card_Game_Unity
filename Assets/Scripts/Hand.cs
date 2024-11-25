dfb9c58e6c1a67f5b3dd893b515b392bacedb741
SteveIsABot <133403738+SteveIsABot@users.noreply.github.com>
2024-11-25 12:54:27 +0000

    Applied Cards display to bot hand aswell

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Hand : MonoBehaviour
 {
     [SerializeField] private List<GameObject> cards = new List<GameObject>();
     [SerializeField] public float sizeToFill;
