332ba68a9f9eafa1b98e14fcf30250679d03ec0e
SteveIsABot <133403738+SteveIsABot@users.noreply.github.com>
2024-11-25 12:58:03 +0000

    Added isBot component
    Bot's hand shouldn't be seen by player

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -6,7 +6,8 @@ public class Hand : MonoBehaviour
 {
     [SerializeField] private List<GameObject> cards = new List<GameObject>();
     [SerializeField] public float sizeToFill;
-    // Update is called once per frame
+    public bool isBot = true;
+
     void Update()
     {
         float startPoint = (-sizeToFill + 0.5f) / 2;
@@ -15,6 +16,12 @@ public class Hand : MonoBehaviour
         Vector3 newPos = new Vector3(startPoint, 0.2f, transform.position.z + 0.1f);
 
         for(int i = 0; i < cards.Count; i++) {
+            
+            if(isBot) {
+                cards[i].GetComponent<Card>().setSuit("Back");
+                cards[i].GetComponent<Card>().updateMat();
+            }
+            
             cards[i].transform.position = newPos;
             newPos.x += placeAmount;
             newPos.y += 0.01f;
